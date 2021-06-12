dfa05890b5b9fb8dbbc2acd47c0f93185e08fe74
szk213 <18608225+szk213@users.noreply.github.com>
2021-06-12 15:32:55 +0900

    add:apigateway-lambda

--- lib/aws-cdk-hands-on-stack.ts
+++ lib/aws-cdk-hands-on-stack.ts
@@ -1,18 +1,16 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
-
+import * as lamda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 export class AwsCdkHandsOnStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'AwsCdkHandsOnQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'AwsCdkHandsOnTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const hello = new lamda.Function(this,'HelloHandler',{
+      runtime: lamda.Runtime.NODEJS_10_X,
+      code: lamda.Code.fromAsset('./lambda'),
+      handler: "hello.handler"
+    })
+    new apigw.LambdaRestApi(this,'Endpoint',{
+      handler: hello
+    })
   }
 }
